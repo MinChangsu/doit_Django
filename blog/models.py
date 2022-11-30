7e1c363c2f3af2819991d7502248ead8001e020e
Changsu <mgs1527@icloud.com>
2022-11-30 15:04:14 +0900

    미리보기와 요약본 추가하기

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,12 @@ import os
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    hook_text = models.CharField(max_length=100, blank=True)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     # author:추후 작성예정
 
