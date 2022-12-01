5183175935b5dd72c39a577d0a75b12fe560bcac
Changsu <mgs1527@icloud.com>
2022-12-01 15:45:13 +0900

    카테고리모델만들기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -13,10 +22,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-
-
-
-
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"[{self.pk}]{self.title} :: {self.author}"
