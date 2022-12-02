812fde566b866a6d851e5493dfe9d360fe0318ac
Changsu <mgs1527@icloud.com>
2022-12-02 16:49:02 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/tests.py
+++ blog/tests.py
@@ -97,6 +97,29 @@ class TestView(TestCase):
         about_me_btn = navbar.find('a', text='About ME')
         self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
+    def test_create_post(self):
+        # 로그인 안하면 status code가 200이면 안됨
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
+        # 로그인함
+        self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Create Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
+
+        self.client.post('/blog/create_post/', {
+            'title': 'Post Form 만들기',
+            'content': 'Post Form 페이지를 만듭시다',
+        })
+        self.assertEqual(Post.objects.count(), 4)
+        last_post = Post.objects.last()
+        self.assertEqual(last_post.title, "Post Form 만들기")
+        self.assertEqual(last_post.author.username, 'trump')
+
     def test_post_list(self):
         # 포스트가 있는경우
         self.assertEqual(Post.objects.count(), 3)
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns =[
+urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.category_page),
-    path('tag/<str:slug>/', views.tag_page)
+    path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
 ]
