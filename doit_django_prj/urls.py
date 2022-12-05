5179fcb42448d48c118c2d5629057e361f91bfaf
Changsu <mgs1527@icloud.com>
2022-12-05 16:18:07 +0900

    구글 로그인 기능 추가하기

--- doit_django_prj/urls.py
+++ doit_django_prj/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('', include("single_pages.urls")),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include("single_pages.urls")),
 
 
 ]
