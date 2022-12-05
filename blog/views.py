9adeebcab3dfe409bf304c4abd8ba3273cf40990
Changsu <mgs1527@icloud.com>
2022-12-05 14:13:59 +0900

    django-crispy-forms 적용하기

--- blog/views.py
+++ blog/views.py
@@ -86,7 +86,7 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
     template_name = 'blog/post_update_form.html'
 
     def get_context_data(self, **kwargs):
