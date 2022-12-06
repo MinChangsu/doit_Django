e1b402f015b39245caa897ee091cb1778dd681aa
Changsu <mgs1527@icloud.com>
2022-12-06 16:05:26 +0900

    댓글 수정기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.core.exceptions import PermissionDenied
@@ -156,5 +156,16 @@ def new_comment(request, pk):
                 return redirect(comment.get_absolute_url())
             else:
                 return redirect(post.get_absolute_url())
+        else:
+            raise PermissionDenied
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
