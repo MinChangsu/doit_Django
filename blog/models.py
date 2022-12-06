e7a3b42924f03289ae108b072eefc2e7cc23e6c7
Changsu <mgs1527@icloud.com>
2022-12-06 18:11:01 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -72,3 +72,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1374/ec5c2026bd411255/svg/guest@email.com{self.author.email}'
